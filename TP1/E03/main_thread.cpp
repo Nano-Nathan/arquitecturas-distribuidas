e0f9994a09b26e6a4d49145a81cf4a0c79eb092c
Nano-Nathan <nanonathan.123@gmail.com>
2022-09-15 20:51:44 -0300

    E03 with thread

--- TP1/E03/main_thread.cpp
+++ TP1/E03/main_thread.cpp
@@ -15,7 +15,8 @@ float suma_total;
 mutex await;
 
 void multiplicate (int start, int end) {
-	float local_result[n][end-start];
+	//float local_result[n][end-start];
+	float local_total = 0;
 	//Recorro las columnas de la matriz 2
 	for (int i = start; i < end; i++){
 		//Recorro las filas de la matriz 1
@@ -27,36 +28,32 @@ void multiplicate (int start, int end) {
 				partial_result += (matriz_1[j][k] * matriz_2[k][i]);
 			}
 			//Guardo el resultado
-			local_result[j][i - start] = partial_result;
+			await.lock();
+			result[j][i] = partial_result;
+			await.unlock();
+			//Sumo el total local
+			local_total += partial_result;
 		}
 	}
+	suma_total += local_total;
 
 	//Bloquea a los demas hilos
-	await.lock();
+	//await.lock();
 	//Recorro las columnas de la matriz
-	for (int i = start; i < end; i++){
-		//Recorro las filas de la matriz
-		for(int j = 0; j < n; j++){
-			//Guardo el resultado
-			result[j][i] = local_result[j][i - start];
-		}
-	}
-	await.unlock();
-}
-
-void add (int start, int end) {
-	float suma_local = 0;
-	for (int i = start; i < end; i++){
-        for (int j = 0; j < n; j++){
-            suma_local += result[i][j];
-        }
-    }
-	suma_total += suma_local;
+	//for (int i = start; i < end; i++){
+	//	//Recorro las filas de la matriz
+	//	for(int j = 0; j < n; j++){
+	//		//Guardo el resultado
+	//		result[j][i] = local_result[j][i - start];
+	//	}
+	//}
+	//await.unlock();
+	//Sumo al total
 }
 
 int main () {
 	timeval time1,time2;
-	thread array_thread[32];
+	int cant_thread = 32;
 
     //inicializo las matrices
     for (int i = 0; i < n; i++){
@@ -70,11 +67,11 @@ int main () {
     gettimeofday(&time1, NULL);
 
 	//Divido los calculos que realizara cada thread
-	int count_col = round(n / 32),
-		extra = n % 32,
+	int count_col = round(n / cant_thread),
+		extra = n % cant_thread,
 		i = 0;
 	//Creo los hilos
-	for (int j = 0; j < 32; j++){
+	for (int j = 0; j < cant_thread; j++){
 		int newValue = i + count_col;
 		//Si sobran columnas, se agrega una a cada thread hasta terminar
 		if(extra > 0){
@@ -82,49 +79,18 @@ int main () {
 			extra--;
 		}
 		if(newValue > n){
-			array_thread[i] = thread(multiplicate, i, n);
+			thread(multiplicate, i, n);
 		} else {
-			array_thread[i] = thread(multiplicate, i, newValue);
+			thread(multiplicate, i, newValue);
 		}
 		i = newValue;
 	}
-	//Espero la ejecucion de los hilos
-    for (int j = 0; j < 32; j++){
-    	if(array_thread[i].joinable() == true){
-    		array_thread[i].join();
-    	}
-    }
-
-	//Sumo el resultado
-	i = 0;
-	extra = n % 32;
-	for(int j = 0; j < 32; j++){
-		int newValue = i + count_col;
-		//Si sobran columnas, se agrega una a cada thread hasta terminar
-		if(extra > 0){
-			newValue++;
-			extra--;
-		}
-		if(newValue > n){
-			array_thread[i] = thread(add, i, n);
-		} else {
-			array_thread[i] = thread(add, i, newValue);
-		}
-		i = newValue;
-	}
-	//Espero la ejecucion de los hilos
-    for (int j = 0; j < 32; j++){
-    	if(array_thread[i].joinable() == true){
-    		array_thread[i].join();
-    	}
-    }
-
-	cout << "Sumatoria: " << suma_total << endl;
 
 	//stop timer
     gettimeofday(&time2, NULL);
 
-    //Matriz resultado
+    //Resultado
+	cout << "Sumatoria: " << suma_total << endl;
 	cout << "Matriz resultado: " << endl;
 	cout << "| " << result[0][0] << " ... " << result[0][n - 1] << " |" << endl;
 	cout << "| " << result[n - 1][0] << " ... " << result[n - 1][n - 1] << " |" << endl;
