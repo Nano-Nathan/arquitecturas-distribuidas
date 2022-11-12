794ca6376e53fc40ee4dce169941b4611f602473
Nano-Nathan <nanonathan.123@gmail.com>
2022-11-12 02:26:38 -0300

    add e02

---
+++ TP3/E02/copy.sh
@@ -0,0 +1,4 @@
+path="/home/hash/serrano_cristian/arquitecturas-distribuidas/TP3/E02"
+file="/home/hash/serrano_cristian/arquitecturas-distribuidas/TP1/E02"
+scp $file/patrones.txt mpi@ip:$path
+scp $file/texto.txt mpi@ip:$path
