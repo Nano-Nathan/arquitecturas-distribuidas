4ecdef5a7e99ef22babbbc1a9b2f07c2b6c87730
Cristian <nanonathan.123@gmail.com>
2022-11-14 10:41:57 -0800

    update excecute

--- execute.sh
+++ execute.sh
@@ -1,9 +1,9 @@
-cd $PATH_SRC
+cd '/home/mpiuser/serrano_cristian/arquitecturas-distribuidas'
 git pull
-cd $PATH_FILE
+cd 'TP3/E01'
 mpicxx -O3 -o main.o main.cpp -O3
-ssh mpiuser@10.65.4.46 'cd $PATH_SRC; git pull;cd $PATH_FILE;mpicxx -O3 -o main.o main.cpp -O3'
-ssh mpiuser@10.65.4.104 'cd $PATH_SRC; git pull;cd $PATH_FILE;mpicxx -O3 -o main.o main.cpp -O3'
-ssh mpiuser@10.65.4.231 'cd $PATH_SRC; git pull;cd $PATH_FILE;mpicxx -O3 -o main.o main.cpp -O3'
-ssh mpiuser@10.65.4.230 'cd $PATH_SRC; git pull;cd $PATH_FILE;mpicxx -O3 -o main.o main.cpp -O3'
-ssh mpiuser@10.65.4.227 'cd $PATH_SRC; git pull;cd $PATH_FILE;mpicxx -O3 -o main.o main.cpp -O3'
+ssh mpiuser@10.65.4.46 "cd '/home/mpiuser/serrano_cristian/arquitecturas-distribuidas'; git pull;cd 'TP3/E01';mpicxx -O3 -o main.o main.cpp -O3"
+ssh mpiuser@10.65.4.104 "cd '/home/mpiuser/serrano_cristian/arquitecturas-distribuidas'; git pull;cd 'TP3/E01';mpicxx -O3 -o main.o main.cpp -O3"
+ssh mpiuser@10.65.4.231 "cd '/home/mpiuser/serrano_cristian/arquitecturas-distribuidas'; git pull;cd 'TP3/E01';mpicxx -O3 -o main.o main.cpp -O3"
+ssh mpiuser@10.65.4.230 "cd '/home/mpiuser/serrano_cristian/arquitecturas-distribuidas'; git pull;cd 'TP3/E01';mpicxx -O3 -o main.o main.cpp -O3"
+ssh mpiuser@10.65.4.227 "cd '/home/mpiuser/serrano_cristian/arquitecturas-distribuidas'; git pull;cd 'TP3/E01';mpicxx -O3 -o main.o main.cpp -O3"
