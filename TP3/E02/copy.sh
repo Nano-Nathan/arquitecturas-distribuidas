fd3aeddb4788380ce876f05f1998c2ee7081ac48
Cristian <nanonathan.123@gmail.com>
2022-11-14 10:55:58 -0800

    update execute and copy

--- TP3/E02/copy.sh
+++ TP3/E02/copy.sh
@@ -2,5 +2,11 @@ path="/home/mpiuser/serrano_cristian/arquitecturas-distribuidas/TP3/E02"
 file="/home/mpiuser/serrano_cristian/arquitecturas-distribuidas/TP1/E02"
 cp $file/patrones.txt $path
 cp $file/texto.txt $path
-scp $file/patrones.txt mpiuser@ip:$path
-scp $file/texto.txt mpiuser@ip:$path
+scp $file/patrones.txt mpiuser@10.65.4.46:$path
+scp $file/texto.txt mpiuser@10.65.4.46:$path
+scp $file/patrones.txt mpiuser@10.65.4.231:$path
+scp $file/texto.txt mpiuser@10.65.4.231:$path
+scp $file/patrones.txt mpiuser@10.65.4.230:$path
+scp $file/texto.txt mpiuser@10.65.4.230:$path
+scp $file/patrones.txt mpiuser@10.65.4.227:$path
+scp $file/texto.txt mpiuser@10.65.4.227:$path
